93cf17c333a10f6aeb2a17cb8f433b5d35015067
graceferguson <grace.c.ferguson@hotmail.com>
2019-10-16 20:27:11 -0600

    Removed Duplicate Code
    Removed calculateTax method from Staff and Faculty + renamed Main.java to EmployeeStatus.java

--- Employee.java
+++ Employee.java
@@ -1,35 +1,33 @@
-
-public class Employee {
-	String employeeName;
-	Department department;
-
-	public Employee(String name, Department department, String section, String section2) {
-		this.employeeName = name;
-		this.department = department;
-
-		if (section.equals("Faculty")) {
-			if (section2.equals("Tenure")) {
-				Tenure a = new Tenure(name, department);
-			} else {
-				Adjunct b = new Adjunct(name, department);
-			}
-		} else {
-
-		}
-	}
-
-	public String getName() {
-		return employeeName;
-	}
-
-	public String getDepartment() {
-		return department.getDepartmentName();
-	}
-
-	public double calculateTaxes(double salary) {
-		double taxedSalary;
-		taxedSalary = salary - (salary * (0.2));
-		return taxedSalary;
-
-	}
-}
+//
+//public class Employee {
+//	String employeeName;
+//	Department department;
+//
+//	public Employee(String name, Department department, String section, String section2) {
+//		this.employeeName = name;
+//		this.department = department;
+//
+//		if (section.equals("Faculty")) {
+//			if (section2.equals("Tenure")) {
+//				Tenure a = new Tenure(name, department);
+//			} else {
+//				Adjunct b = new Adjunct(name, department);
+//			}
+//		}
+//	}
+//
+//	public String getName() {
+//		return employeeName;
+//	}
+//
+//	public String getDepartment() {
+//		return department.getDepartmentName();
+//	}
+//
+//	public double calculateTaxes(double salary) {
+//		double taxedSalary;
+//		taxedSalary = salary - (salary * (0.2));
+//		return taxedSalary;
+//
+//	}
+//}
