6eebb5713a1c1a72ef85c84bac5b229e13e62c2f
graceferguson <grace.c.ferguson@hotmail.com>
2019-10-16 19:54:47 -0600

    Remove Parameter
    Removed "department" parameter from Staff constructor since Staff no longer extends Employee, and staff should not have a department.

--- Main.java
+++ Main.java
@@ -8,7 +8,15 @@ public class Main {
 
 		Faculty steve = new Faculty("Steve", art, "Adjunct");
 		Faculty marg = new Faculty("Marg", science, "Tenure");
-		Staff pat = new Staff("Pat", null, "Staff", "what");
+		Staff pat = new Staff("Pat");
+
+	
+		//keyboard input would create an object and give it a section
+//		if (section.equals("Tenure")) {
+//			Tenure a = new Tenure(name, department, section);
+//		} else {
+//			Adjunct b = new Adjunct(name, department, section);
+//		}
 
 	}
 
--- Staff.java
+++ Staff.java
@@ -1,17 +1,10 @@
 
 public class Staff {
 	String staffName;
-	Department staffDepartment;
-	String staffSection;
-	String staffSection2;
 
-	public Staff(String name, Department department, String section, String section2) {
+	public Staff(String name) {
 		this.staffName = name;
-		this.staffDepartment = department;
-		this.staffSection = section;
-		this.staffSection2 = section2;
 		
-
 	}
 
 	public double calculateTaxes(double salary) {
