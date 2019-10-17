c2c9ed66c69bde14c846553ea4fe9c7040c6e54d
graceferguson <grace.c.ferguson@hotmail.com>
2019-10-16 20:04:36 -0600

    Tests to reflect changes
    New tests to reflect removing Employee.java and removing parameter from Staff.java

--- RefactorTest.java
+++ RefactorTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 public class RefactorTest {
 
 	Department testDepartment = new Department("blank");
-	Tenure testTenure = new Tenure("Tony", testDepartment, "");
+	Tenure testTenure = new Tenure("Tony", testDepartment, "Tenure");
+	Adjunct testAdjunct = new Adjunct("Charles", testDepartment, "Adjunct");
+	Staff testStaff = new Staff("Jan");
+	
 	
 	@Before
 	public void setUp() throws Exception {
@@ -20,6 +23,21 @@ public class RefactorTest {
 	@Test
 	public void raiseTest() {
 		assertEquals(testTenure.getRaise(50000.0, 8.0), 90000, 0.0);
+	}
+	
+	@Test
+	public void tenureTest(){
+		assertEquals(testTenure.getName(), "Tony");
+	}
+	
+	@Test
+	public void adjunctTest(){
+		assertEquals(testAdjunct.getName(), "Charles");
+	}
+	
+	@Test
+	public void staffTest(){
+		assertEquals(testStaff.getName(testStaff), "Jan");
 	}
 
 }
